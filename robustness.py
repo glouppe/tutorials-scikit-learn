2d26b3d95f68dff071787794c561ad582e19dacc
Gilles Louppe <g.louppe@gmail.com>
2016-04-11 15:02:39 +0200

    save

--- robustness.py
+++ robustness.py
@@ -1,6 +1,7 @@
-from matplotlib import pyplot as plt
-from sklearn.tree import DecisionTreeClassifier
 import numpy as np
+from matplotlib import pyplot as plt
+from scipy import stats
+from sklearn.tree import DecisionTreeClassifier
 
 
 def plot_surface(model, X, y):
@@ -35,5 +36,32 @@ def plot_surface(model, X, y):
     for i, c in zip(range(n_classes), plot_colors):
         idx = np.where(y == i)
         plt.scatter(X[idx, 0], X[idx, 1], c=c, cmap=cmap)
+
+    plt.show()
+
+
+def plot_outlier_detector(clf, X, ground_truth, outliers_fraction, n_outliers):
+    xx, yy = np.meshgrid(np.linspace(-7, 7, 500), np.linspace(-7, 7, 500))
+
+    y_pred = clf.decision_function(X).ravel()
+    threshold = stats.scoreatpercentile(y_pred, 100 * outliers_fraction)
+    y_pred = y_pred > threshold
+    n_errors = (y_pred != ground_truth).sum()
+
+    Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    plt.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),
+                 cmap=plt.cm.Blues_r)
+    a = plt.contour(xx, yy, Z, levels=[threshold],
+                    linewidths=2, colors='red')
+    plt.contourf(xx, yy, Z, levels=[threshold, Z.max()], colors='orange')
+    b = plt.scatter(X[:-n_outliers, 0], X[:-n_outliers, 1], c='white')
+    c = plt.scatter(X[-n_outliers:, 0], X[-n_outliers:, 1], c='black')
+    plt.legend(
+        [a.collections[0], b, c],
+        ['learned decision function', 'true inliers', 'true outliers'])
+    plt.xlabel("errors: %d" % n_errors)
+    plt.xlim((-7, 7))
+    plt.ylim((-7, 7))
 
     plt.show()
